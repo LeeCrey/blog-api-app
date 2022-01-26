607c6061d8cea22a201b8ae0127778ea0e5625b6
LeeCrey <SolomonBoloshe11x@gmail.com>
2022-01-26 04:48:50 -0500

    exception handler

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -4,53 +4,40 @@ module ExceptionHandler
   extend ActiveSupport::Concern
 
   included do
-    rescue_from UnpermittedException, with: :unpermitted_exception_handler
+    rescue_from UnpermittedException, with: :unpermitted_exception
     rescue_from UnauthorizedException, with: :unauthorized_exception
     rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
     rescue_from CommentNotFoundException, with: :comment_not_found
     rescue_from PostNotFoundException, with: :post_not_found
-    rescue_from InvalidTokenException, with: :invalid_token_exception_handler
+    rescue_from InvalidTokenException, with: :invalid_token_exception
   end
 
   private
 
   # Handlers
-  def unpermitted_exception_handler
-    render_error_response 'Forbidden', :forbidden
+  def unpermitted_exception
+    render json: { error: 'Forbidden' }, status: :forbidden
   end
 
-  def invalid_token_exception_handler
-    msg = 'Invalid authorization token'
-    render_error_response msg, :unauthorized
+  def invalid_token_exception
+    render json: { error: 'Invalid authorization token' }, status: :unauthorized
   end
 
   def post_not_found
-    msg = %(Post not found)
-    render_error_response msg, :not_found
+    render json: { error: 'Post not found' }, status: :not_found
   end
 
   def comment_not_found
-    msg = %(Comment not found)
-    render_error_response msg, :not_found
+    render json: { error: 'Comment not found' }, status: :not_found
   end
 
   def record_invalid
-    render_error_response 'Invalid data', :unprocessable_entity
+    render json: { error: 'Invalid data' }, status: :unprocessable_entity
   end
 
   # responses
-
-  def record_not_found
-    render_error_response 'Not found', :not_found
-  end
-
   def unauthorized_exception
-    render_error_response 'Unauthorized Request', :unauthorized
-  end
-
-  ##
-  def render_error_response(msg, status)
-    render json: { error: msg }, status: status
+    render json: { error: 'Unauthorized request' }, status: :unauthorized
   end
 end
