099f3532901c1379b987ade66350e26af6f36928
LeeCrey <SolomonBoloshe11x@gmail.com>
2022-01-26 04:46:15 -0500

    comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,7 +22,7 @@ class CommentsController < ApplicationController
     @comment.user_id = @current_user.id
 
     if @comment.save
-      render json: @comment, status: :created, location: @comment
+      render json: @comment, status: :created
     else
       render json: @comment.errors, status: :unprocessable_entity
     end
