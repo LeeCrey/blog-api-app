c210e75bfb4d19afa3eac7cbbc3d9bc2fb9b070e
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 14:24:38 +0300

    render :create

--- app/controllers/api/v1/android/posts_controller.rb
+++ app/controllers/api/v1/android/posts_controller.rb
@@ -20,7 +20,7 @@ class Api::V1::Android::PostsController < ApplicationController
     @post = current_user.posts.new(post_params)
 
     if @post.save
-      render json: @post, status: :created
+      render :create, status: :created
     else
       render json: @post.errors, status: :unprocessable_entity
     end
