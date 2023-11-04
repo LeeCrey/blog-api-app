592cb2d861ac7289623d753c84970782b95dadeb
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 11:46:00 +0300

    modified

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins "localhost:3000"
 
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource "/api/*",
+      headers: %w(Authorization),
+      methods: :any,
+      expose: %w(Authorization),
+      max_age: 600
   end
 end
