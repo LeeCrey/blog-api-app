68df2ffefd87935b89102764de5354c471b9b0bf
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 10:46:32 +0300

    moved relation to concern

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts, dependent: :destroy
-  has_many :comments, dependent: :destroy
-
   include UserConcern
 end
