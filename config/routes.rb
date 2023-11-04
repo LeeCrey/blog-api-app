f42657c82b924d075d2a48fe1d1660e553314be9
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 11:17:14 +0300

    excluded show action for both post and comment

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,8 @@ Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
         namespace :android do
-          resources :posts, shallow: true do
-            resources :comments
+          resources :posts, except: %i[show], shallow: true do
+            resources :comments, except: %i[show]
           end
         end
       end
