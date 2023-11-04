9f2a05a90148049aad98598801939e39d59b5052
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 10:34:29 +0300

    last seen at

--- db/migrate/20211229075018_create_users.rb
+++ db/migrate/20211229075018_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :first_name, limit: 30, null: false
       t.string :last_name, limit: 30
+      t.datetime :last_seen_at
 
       t.timestamps
     end
