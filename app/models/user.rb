3f17c01e821a5ad2a15fe24f32a56ead0dfe87fb
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 10:53:29 +0300

    jwt authenticable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@
 class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
   include UserConcern
 end
