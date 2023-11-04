ef083fc86b6c39166e4fe9732d9af3885442913b
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 10:16:27 +0300

    implemented logic

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 class PostPolicy < ApplicationPolicy
-  class Scope < Scope
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+  private
+
+  def check
+    @record.user_id == @user.id
   end
 end
