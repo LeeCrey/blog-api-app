417f0cb1f20f17c188e4aec2d5ffbe9414ba1e20
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 10:37:47 +0300

    bug fix

--- app/models/concerns/user_concern.rb
+++ app/models/concerns/user_concern.rb
@@ -4,21 +4,12 @@ module UserConcern
   extend ActiveSupport::Concern
 
   included do
-    validates :email, :first_name, :password, presence: true
-    validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-    validates :first_name, length: { within: 1..30 }
+    validates :first_name, presence: true, length: { within: 1..30 }
     # last can be nil but if it exist, its length should not exceed 30 chars
     validates :last_name, length: { within: 1..30 }, if: -> { last_name.present? }
-    validates :password, length: { minimum: 6 }
-
-    has_secure_password
   end
 
-  def token_get
-    JsonWebToken.encode({ email: email })
-  end
-
-  def full_name
+  def to_s
     return first_name if last_name.blank?
 
     "#{first_name} #{last_name}"
