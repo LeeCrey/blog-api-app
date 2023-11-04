3fd346d3dfddac45c04669d28d85fe0c653faee4
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 10:51:04 +0300

    deny list

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_01_02_104002) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_04_064231) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "post_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -24,7 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_01_02_104002) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
@@ -33,10 +36,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_01_02_104002) do
   create_table "users", force: :cascade do |t|
     t.string "first_name", limit: 30, null: false
     t.string "last_name", limit: 30
-    t.string "email", null: false
-    t.string "password_digest", null: false
+    t.datetime "last_seen_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "posts"
