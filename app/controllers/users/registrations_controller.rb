70c9502924e9023fc58558e050f087e6fd7314d6
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 10:21:58 +0300

    included concern

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
- 
+  protect_from_forgery with: :null_session
+  respond_to :json
+
+  include RegistrationsConcern
 end
