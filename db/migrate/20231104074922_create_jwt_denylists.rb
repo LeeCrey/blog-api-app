debf92e3092c8c8d4dcff7f0c51acb2a9b716e5e
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 10:50:20 +0300

    generated

---
+++ db/migrate/20231104074922_create_jwt_denylists.rb
@@ -0,0 +1,13 @@
+class CreateJwtDenylists < ActiveRecord::Migration[7.1]
+  def change
+    create_table :jwt_denylists do |t|
+      t.string :jti, null: false
+      t.datetime :exp, null: false
+
+      t.timestamps
+    end
+
+    add_index :jwt_denylists, :jti, unique: true
+    add_index :jwt_denylists, %i[jti exp]
+  end
+end
