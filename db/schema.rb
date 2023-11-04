fb4017eb77ceeb0cd07fcfda23ccc5c1ba5e740d
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 14:42:31 +0300

    votes and replies count for comment

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_04_091533) do
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
     t.text "content"
+    t.integer "cached_votes_up", default: 0, null: false
+    t.integer "replies_count", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
