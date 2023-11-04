d60f1f18c62b9782d0a9f6180a105273f8c6a9d7
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 12:02:42 +0300

    comments and votes count

--- db/migrate/20211229075051_create_posts.rb
+++ db/migrate/20211229075051_create_posts.rb
@@ -1,9 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.string :title
       t.text :content, null: false
       t.belongs_to :user, null: false, foreign_key: true
+      t.integer :cached_votes_up, null: false, default: 0
+      t.integer :comments_count, null: false, default: 0
 
       t.timestamps
     end
