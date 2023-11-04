736f755fd389c9002093ff9fc3619b313691390b
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 09:46:09 +0300

    removed email field

--- db/migrate/20211229075018_create_users.rb
+++ db/migrate/20211229075018_create_users.rb
@@ -3,8 +3,6 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :first_name, limit: 30, null: false
       t.string :last_name, limit: 30
-      t.string :email, null: false
-      t.string :password_digest, null: false
 
       t.timestamps
     end
