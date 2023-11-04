9beef553bf2acb5802477f46e596ac5416c89aa4
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 14:48:57 +0300

    handle param missing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::API
     render_json_error(ex.message, :unprocessable_entity)
   end
 
+  rescue_from ActionController::ParameterMissing do |ex|
+    render_json_error(ex.message, :bad_request)
+  end
+
   protected
 
   def update_last_seen_at
@@ -22,6 +26,6 @@ class ApplicationController < ActionController::API
   def raise_if_blank(resource, name)
     return if resource.present?
 
-    raise ActiveRecord::RecordNotFound, t('not_found', resource: name)
+    raise ActiveRecord::RecordNotFound, t("not_found", resource: name)
   end
 end
