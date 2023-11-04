0d238158594ed6bfb4125caa8e5ae581a409e98d
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 12:09:36 +0300

    removed protect from forgery

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  protect_from_forgery with: :null_session
   respond_to :json
 
   include RegistrationsConcern
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # protect_from_forgery with: :null_session
   respond_to :json
 
   include SessionsConcern
