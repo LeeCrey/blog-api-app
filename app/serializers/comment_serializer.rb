e4a68a440d87bc9e6dd0b91d6159dfecc498f971
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 10:38:45 +0300

    removed

--- app/serializers/comment_serializer.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :content, :created_at
-
-  belongs_to :user
-end
--- app/serializers/post_serializer.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :created_at, :comments
-
-  belongs_to :user
-  has_many :comments
-end
