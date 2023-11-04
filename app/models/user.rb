0810bfca7512a859cd435e2bfc0188b429789cc0
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 10:31:00 +0300

    devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
