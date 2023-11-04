d7cbd32d1e9e980dd631bf2d86e6dc3ef260cbd7
lee_crey_116 <solomonboloshe835@gmail.com>
2023-11-04 10:52:23 +0300

    setup

--- app/models/jwt_denylist.rb
+++ app/models/jwt_denylist.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class JwtDenylist < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::Denylist
+
+  # Validations
+  validates :jti, :exp, presence: true
 end
